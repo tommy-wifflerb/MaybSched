3116a87ab687d2626c9cf5b9517e44e6e6b93af7
Thomas Wiffler <thomas@thomas.lan1>
2020-02-25 22:30:34 -0600

    Updated sched1 file

--- sched1.py
+++ sched1.py
@@ -1,41 +1,41 @@
 import csv
 import numpy as np
 import pandas as pd
-
+# Load data into dataframe
 data = pd.read_csv('Holiday Schedule Ranking 2019.csv', index_col = 0)
+print(data)
 cols = []
+# Get column headers
 for col in data.columns:
     cols.append(col)
+print(cols)
 numdates = len(cols)
-
+print(numdates)
+# Make empty schedule
 schedule = pd.DataFrame(np.full((3,numdates), "replace"), columns = cols)
-
-positions = ["one","two","three"]
-
+positions = ["One","Two","Three"]
 schedule.index = positions
+print(schedule)
 
 empdaysdict = {}
-
+# This is where we figure out the schedule
 for emp, row in data.iterrows():
-    day_count = 0
+    workcount = 0
     sortedlistofdates = row.sort_values(axis=0).index.tolist()
-    for x in range(13):
-        if day_count == 2:
+    for x in range(numdates):
+        if workcount == 2:
             break
+        if schedule.loc["One", sortedlistofdates[x]] == "replace":
+            schedule.loc["One", sortedlistofdates[x]] = emp
+            workcount = workcount + 1
+        elif schedule.loc["Two", sortedlistofdates[x]] == "replace":
+            schedule.loc["Two", sortedlistofdates[x]] = emp
+            workcount = workcount + 1
+        elif schedule.loc["Three", sortedlistofdates[x]] == "replace":
+            schedule.loc["Three", sortedlistofdates[x]] = emp
+            workcount = workcount + 1
+        empdaysdict[emp] = workcount
 
-        if schedule.loc["one", sortedlistofdates[x]] == "replace":
-            schedule.loc["one", sortedlistofdates[x]] = emp
-            day_count +=1
-
-        elif schedule.loc["two", sortedlistofdates[x]] == "replace":
-            schedule.loc["two", sortedlistofdates[x]] = emp
-            day_count +=1
-        
-        elif schedule.loc["three", sortedlistofdates[x]] == "replace":
-            schedule.loc["three", sortedlistofdates[x]] = emp
-            day_count +=1
-
-        empdaysdict[emp] = day_count
-
-# then write dataframe to csv file
-schedule.to_csv("final_schedule.csv")
+print(empdaysdict)
+# write dataframe to csv file
+schedule.to_csv("Final_Mayb_Schedule.csv")
